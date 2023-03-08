a0d96e123cd34a6e9b888865366304b3abc4bf43
JARAYTIDA <rajadityakumawat@gmail.com>
2023-03-08 16:03:08 +0530

    created context file

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -5,6 +5,37 @@ import {ethers} from 'ethers';
 const StateContext = createContext();
 
 export const StateContextProvider = ({children}) => {
-    const {conntract} = useContract('0xcb58dd7bcd7ADC4d161DbA8575a1d0900563BC23');
+    const {contract} = useContract('0xcb58dd7bcd7ADC4d161DbA8575a1d0900563BC23');
     const {mutateAsync: createCampaign} = useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async(form) => {
+        try {
+            const data = await createCampaign([
+                address, //owner
+                form.title, //title
+                form.description, //description
+                form.target,
+                new Date(from.deadline).getTime(),
+                form.image
+            ])  
+
+            console.log("contract call success", data)
+
+        } catch (error) {
+
+            console.log("contract call failed",error)
+            
+        }
+
+    }
+
+    return(
+        <StateContext.Provider value={{address, contract, createCampaign:publishCampaign}}>
+            {children}
+        </StateContext.Provider>
+    )
+
 }
